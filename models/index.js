1c231a6a083b9771a0a80b3cb783bc61b33d2838
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-04 16:04:50 -0400

    added routes for category get, post, delete

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
@@ -18,10 +18,14 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag});
+Product.belongsToMany(Tag, {through: ProductTag}, {
+  foreignKey:'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag});
+Tag.belongsToMany(Product, {through: ProductTag}, {
+  foreignKey:'tag_id',
+});
 
 module.exports = {
   Product,
