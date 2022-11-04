b175d09fb566bf0ae2a0ea2cdbd9b5a91089f0aa
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-04 15:52:27 -0400

    added association methods to create relationships between models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// association methods to create relationships between models
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,
